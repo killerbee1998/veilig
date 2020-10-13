3894e022cbee56e48f5fb9daba04c1ee2eed69da
riasat1998 <riasat1998@gmail.com>
2020-10-13 19:08:24 +0600

    passCharCode is now always an int instead of a float

--- app.js
+++ app.js
@@ -26,22 +26,22 @@ app.get('/pass/len=:len/flags=:flags', (req, res) =>{
     }
 
     for(let i=0;i<len;++i){
-        passCharCode = Math.random() * (max - min) + min
+        passCharCode = Math.floor(Math.random() * (max - min + 1) + min)
 
         // if special flag is off 
         if(if_specials === 0){
 
             let p = [0,0,0]
             if(if_nums !== 0){
-                p[0] = Math.random() * (57 - 48) + 48
+                p[0] = Math.floor(Math.random() * (57 - 48+1) + 48)
             }
 
             if(if_caps !== 0){
-                p[1] = Math.random() * (90 - 65) + 65
+                p[1] = Math.floor(Math.random() * (90 - 65+1) + 65)
             }
 
-            p[2] = Math.random() * (122 - 97) + 97
-            console.log(p)
+            p[2] = Math.floor(Math.random() * (122 - 97+1) + 97)
+            
             
             // if both numbers and caps are o on
             if(p[0] !== 0 && p[1] !== 0){
@@ -85,15 +85,15 @@ app.get('/pass/len=:len/flags=:flags', (req, res) =>{
 
             let p = [0,0,0,0]
             if(if_nums !== 0){
-                p[0] = Math.random() * (57 - 48) + 48
+                p[0] = Math.floor(Math.random() * (57 - 48 + 1) + 48)
             }
 
             if(if_specials !== 0 ){
-                p[1] = Math.random() * (58 - 64) + 58
-                p[2] = Math.random() * (96 - 91) + 91
+                p[1] = Math.floor(Math.random() * (58 - 64 + 1) + 58)
+                p[2] = Math.floor(Math.random() * (96 - 91 + 1) + 91)
             }
 
-            p[3] = Math.random() * (122 - 97) + 97
+            p[3] = Math.floor(Math.random() * (122 - 97 + 1) + 97)
 
             // if both numbers and symbols flags are on
             if(p[0] !== 0 && p[1] !== 0 && p[2] !== 0){
@@ -135,7 +135,7 @@ app.get('/pass/len=:len/flags=:flags', (req, res) =>{
             
             
         }
-
+        
         pass += String.fromCharCode(passCharCode)
     }
     res.status(200).json(pass)

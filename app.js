2b70df10646e85cdeac855b5104bb53ac31ae853
riasat1998 <riasat1998@gmail.com>
2020-10-13 16:58:46 +0600

    started on main pass func

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 const express = require("express")
 const app = express()
 
+// body parser
+const bodyparser = require('body-parser')
+app.use(bodyparser)
+
 app.get('/', (req, res) =>{
     res.status(200).json("This is root path")
+})
+
+app.get('/pass/len=:len/flags=:flags', (req, res) =>{
+    const {len, flags} = req.params
 })

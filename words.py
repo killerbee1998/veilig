975ea50b3729bef2d788fca5d8dac4e878a86f4a
riasat1998 <riasat1998@gmail.com>
2020-10-16 21:45:41 +0600

    refactored words.py as a reusable function

--- words.py
+++ words.py
@@ -1,30 +1,39 @@
-f1 = open('wordlist/wordlist_UK.txt')
-data = f1.read().split('\n')
-words_1 = []
+fnames = ['wordlist/wordlist_UK.txt', 'wordlist/wordlist_US.txt']
 
-for i in range(len(data)):
+def word_lists(fname):
+    f1 = open(fname)
+    data = f1.read().split('\n')
+    words = []
 
-    acceptable  = True
+    for i in range(len(data)):
 
-    # dont accept words less than four letters long
-    if(len(data[i]) < 4):
-        acceptable = False
+        acceptable  = True
 
-    # dont accept words with any punctuation or capitals
-    for j in range(len(data[i])):
-        if (data[i][j] <='a' or data[i][j]>='z'):
-            acceptable = False  
+        # dont accept words less than four letters long
+        if(len(data[i]) < 4):
+            acceptable = False
 
-    # dont accept similar words(same word but with ing, 's etc. EX: york and york's)
-    if('ing' in data[i][-3:]):
-        acceptable = False
-    if('est' in data[i][-3:]):
-        acceptable = False
-        
-    if(i>0 and data[i][:-1] == data[i-1] and data[i][-1] == 's'):
-        acceptable = False
-        
-    if(acceptable):
-        words_1.append(data[i])
+        # dont accept words with any punctuation or capitals
+        for j in range(len(data[i])):
+            if (data[i][j] <='a' or data[i][j]>='z'):
+                acceptable = False  
 
-print(words_1)
+        # dont accept similar words(same word but with ing, 's etc. EX: york and york's)
+        if('ing' in data[i][-3:]):
+            acceptable = False
+        if('est' in data[i][-3:]):
+            acceptable = False
+            
+        if(i>0 and data[i][:-1] == data[i-1] and data[i][-1] == 's'):
+            acceptable = False
+            
+        if(acceptable):
+            words.append(data[i])
+    
+    return set(words)
+
+all_words = set([])
+for i in fnames:
+    all_words.update(word_lists(i))
+
+print(all_words)

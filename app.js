5bb003b59716226d52e7bf9158c5a0697a04a43e
riasat1998 <riasat1998@gmail.com>
2020-10-17 20:30:01 +0600

    created a new passphrase func

--- app.js
+++ app.js
@@ -8,13 +8,29 @@ app.get('/', (req, res) =>{
     res.status(200).json("This is root path")
 })
 
-// pass func
+// password func
 app.get('/pass/len=:len/passFlags=:flags', (req, res) =>{
     let {len, flags} = req.params
     const passgen = require('./passgen')
     const pass = passgen.passgen(len, flags)
 
     res.status(200).json(pass)
+})
+
+
+// passphrase func
+app.get('/passphrase/n_words=:n_words', (req, res)=>{
+    let {n_words} = req.params
+    const get_word = require('./get_word')
+
+    let passphrase = ''
+    for(let i=0;i<n_words;++i){
+        passphrase += get_word.get_random_word()+'-'
+    }
+
+    res.status(200).json(passphrase)
+
+
 })
 
 module.exports.app = app

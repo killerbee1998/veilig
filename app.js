a70f4204caa6622e0a0b8751c5f0bd427e59515a
riasat1998 <riasat1998@gmail.com>
2020-10-17 21:46:01 +0600

    Added alternate routes for passphrase function

--- app.js
+++ app.js
@@ -49,9 +49,50 @@ app.get('/passphrase/n_words=:n_words/passFlags=:flags', (req, res)=>{
     const get_word = require('./get_word')
 
     let passphrase = ''
-    for(let i=0;i<n_words;++i){
+    for(let i=0;i<n_words-1;++i){
         passphrase += get_word.get_random_word(flags)+'-'
     }
+    passphrase += get_word.get_random_word(flags)
+
+    res.status(200).json(passphrase)
+})
+
+// passphrase function(alt routes)
+app.get('/passphrase/n_words=:n_words', (req, res)=>{
+    let {n_words, flags} = req.params
+    const get_word = require('./get_word')
+
+    let passphrase = ''
+    for(let i=0;i<n_words-1;++i){
+        passphrase += get_word.get_random_word('000')+'-'
+    }
+    passphrase += get_word.get_random_word('000')
+
+    res.status(200).json(passphrase)
+})
+
+
+app.get('/passphrase/passFlags=:flags', (req, res)=>{
+    let {flags} = req.params
+    const get_word = require('./get_word')
+
+    let passphrase = ''
+    for(let i=0;i<2;++i){
+        passphrase += get_word.get_random_word(flags)+'-'
+    }
+    passphrase += get_word.get_random_word(flags)
+
+    res.status(200).json(passphrase)
+})
+
+app.get('/passphrase/', (req, res)=>{
+    const get_word = require('./get_word')
+
+    let passphrase = ''
+    for(let i=0;i<2;++i){
+        passphrase += get_word.get_random_word('000')+'-'
+    }
+    passphrase += get_word.get_random_word('000')
 
     res.status(200).json(passphrase)
 })

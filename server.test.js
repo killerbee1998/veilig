5351a3d8ae7335f8153c9474581381fec813e1dc
riasat1998 <riasat1998@gmail.com>
2020-10-22 21:15:38 +0600

    server.test: pass/passFlags regular tests

--- server.test.js
+++ server.test.js
@@ -32,3 +32,47 @@ describe('pass/len', () =>{
         expect(res.statusCode).toBe(400)
     })
 })
+
+describe('pass/passFlags', () =>{
+
+    test("flags=000", async () =>{
+        const res = await req(app).get('/pass/passFlags=000/')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test("flags=001", async () =>{
+        const res = await req(app).get('/pass/passFlags=001/')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test("flags=010", async () =>{
+        const res = await req(app).get('/pass/passFlags=010/')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test("flags=011", async () =>{
+        const res = await req(app).get('/pass/passFlags=011/')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test("flags=100", async () =>{
+        const res = await req(app).get('/pass/passFlags=100/')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test("flags=101", async () =>{
+        const res = await req(app).get('/pass/passFlags=101/')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test("flags=110", async () =>{
+        const res = await req(app).get('/pass/passFlags=110/')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test("flags=111", async () =>{
+        const res = await req(app).get('/pass/passFlags=111/')
+        expect(res.statusCode).toBe(200)
+    })
+
+})

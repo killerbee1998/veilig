665581a206abf143eae618ec62c639dd05de4e0c
riasat1998 <riasat1998@gmail.com>
2020-10-22 21:11:43 +0600

    server.test: pass/len paths

--- server.test.js
+++ server.test.js
@@ -7,3 +7,28 @@ describe('Root path', () =>{
         expect(res.statusCode).toBe(200)      
     })
 })
+
+describe('pass/', () =>{
+    test("pass/ with no args", async () =>{
+        const res = await req(app).get('/pass/')
+        expect(res.statusCode).toBe(200)
+    })
+})
+
+describe('pass/len', () =>{
+    test("pass/len with len=5", async () =>{
+        const res = await req(app).get('/pass/len=5/')
+        expect(res.statusCode).toBe(200)
+    })
+
+    test("pass/len with len<5", async () =>{
+        const res = await req(app).get('/pass/len=2/')
+        expect(res.statusCode).toBe(400)
+    })
+
+
+    test("pass/len with len>200", async () =>{
+        const res = await req(app).get('/pass/len=220/')
+        expect(res.statusCode).toBe(400)
+    })
+})

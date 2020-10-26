e6e979b3224d1db85308db889a790c5ac9bce468
riasat1998 <riasat1998@gmail.com>
2020-10-26 20:08:59 +0600

    app: register function done

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ const knex = require('knex')
 const pg = knex({
   client: 'pg',
   connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: true
+      connectionString: process.env.DATABASE_URL
   }
 });
 

ad8e3ab0b7bd74f800f138f5c42185be0784096c
riasat1998 <riasat1998@gmail.com>
2020-10-30 20:39:56 +0600

    passphraseRoutes: moved all passphrase/  routes  to passphraseRoutes module

--- src/app.js
+++ src/app.js
@@ -24,81 +24,12 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 // load routes
-let passPaths = require('./passRoutes')
+const passPaths = require('./passRoutes')
 app.use('/pass', passPaths)
-
-const {get_random_word} = require('./gen_pass/getWord')
-const {passFlagsChecker} = require('./gen_pass/passFlagsChecker')
 
 // root 
 app.get('/', (req, res) => {
     res.status(200).json("This is root path")
-})
-
-// passphrase func
-app.get('/passphrase/n_words=:n_words/passFlags=:flags', (req, res) => {
-    let {n_words,flags} = req.params
-
-    if (n_words < 3 || n_words > 20) {
-        res.send(400).json("Length out of bounds")
-    }
-
-    if(!(passFlagsChecker(flags))){
-        res.send(400).json("Incorrect passFlags format")
-    }
-
-    let passphrase = ''
-    for (let i = 0; i < n_words - 1; ++i) {
-        passphrase += get_random_word(flags) + '-'
-    }
-    passphrase += get_random_word(flags)
-
-    res.status(200).json(passphrase)
-})
-
-// passphrase function(alt routes)
-app.get('/passphrase/n_words=:n_words', (req, res) => {
-    let {n_words} = req.params
-
-    if (n_words < 3 || n_words > 20) {
-        res.send(400).json("Length out of bounds")
-    }
-
-    let passphrase = ''
-    for (let i = 0; i < n_words - 1; ++i) {
-        passphrase += get_random_word('000') + '-'
-    }
-    passphrase += get_random_word('000')
-
-    res.status(200).json(passphrase)
-})
-
-
-app.get('/passphrase/passFlags=:flags', (req, res) => {
-    let {flags} = req.params
-    
-    if(!(passFlagsChecker(flags))){
-        res.send(400).json("Incorrect passFlags format")
-    }
-
-    let passphrase = ''
-    for (let i = 0; i < 2; ++i) {
-        passphrase += get_random_word(flags) + '-'
-    }
-    passphrase += get_random_word(flags)
-
-    res.status(200).json(passphrase)
-})
-
-app.get('/passphrase/', (req, res) => {
-
-    let passphrase = ''
-    for (let i = 0; i < 2; ++i) {
-        passphrase += get_random_word('000') + '-'
-    }
-    passphrase += get_random_word('000')
-
-    res.status(200).json(passphrase)
 })
 
 // user signin function
---
+++ src/passphraseRoutes.js
@@ -0,0 +1,75 @@
+// express
+const express = require("express")
+const passphraseRoutes = express.Router()
+
+// local modules
+const {get_random_word} = require('./gen_pass/getWord')
+const {passFlagsChecker} = require('./gen_pass/passFlagsChecker')
+
+// passphrase func
+app.get('/n_words=:n_words/passFlags=:flags', (req, res) => {
+    let {n_words,flags} = req.params
+
+    if (n_words < 3 || n_words > 20) {
+        res.send(400).json("Length out of bounds")
+    }
+
+    if(!(passFlagsChecker(flags))){
+        res.send(400).json("Incorrect passFlags format")
+    }
+
+    let passphrase = ''
+    for (let i = 0; i < n_words - 1; ++i) {
+        passphrase += get_random_word(flags) + '-'
+    }
+    passphrase += get_random_word(flags)
+
+    res.status(200).json(passphrase)
+})
+
+// passphrase function(alt routes)
+app.get('/n_words=:n_words', (req, res) => {
+    let {n_words} = req.params
+
+    if (n_words < 3 || n_words > 20) {
+        res.send(400).json("Length out of bounds")
+    }
+
+    let passphrase = ''
+    for (let i = 0; i < n_words - 1; ++i) {
+        passphrase += get_random_word('000') + '-'
+    }
+    passphrase += get_random_word('000')
+
+    res.status(200).json(passphrase)
+})
+
+
+app.get('/passFlags=:flags', (req, res) => {
+    let {flags} = req.params
+    
+    if(!(passFlagsChecker(flags))){
+        res.send(400).json("Incorrect passFlags format")
+    }
+
+    let passphrase = ''
+    for (let i = 0; i < 2; ++i) {
+        passphrase += get_random_word(flags) + '-'
+    }
+    passphrase += get_random_word(flags)
+
+    res.status(200).json(passphrase)
+})
+
+app.get('/', (req, res) => {
+
+    let passphrase = ''
+    for (let i = 0; i < 2; ++i) {
+        passphrase += get_random_word('000') + '-'
+    }
+    passphrase += get_random_word('000')
+
+    res.status(200).json(passphrase)
+})
+
+module.exports = passphraseRoutes

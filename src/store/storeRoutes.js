65c210af2c4983a6f7513739ed07ed0ec07c8437
riasat1998 <riasat1998@gmail.com>
2020-11-03 19:34:35 +0600

    store/storeRoutes: /savePasss function uses jwt to confirm login

--- src/store/storeRoutes.js
+++ src/store/storeRoutes.js
@@ -8,6 +8,22 @@ const knex = require('knex')
 // json web token
 const jwt = require('jsonwebtoken')
 
+storeRoutes.post('/savePass', (req,res)=>{
+    const {user_url, user_name, user_pass, token, authKey} = req.body
+    
+    try{
+        let master_email = jwt.verify(token)
+        
+        const pg = knex({
+            client: 'pg',
+            connection: {
+                connectionString: process.env.DATABASE_URL
+            }
+        });
+        
+    }catch{
 
+    }
+})
 
 module.exports = storeRoutes
